eb6bea0ce429f6baad60f1cf3fc7963b9879132f
Willow011 <99154745+Willow011@users.noreply.github.com>
2023-02-04 19:51:37 -0800

    Add files via upload

---
+++ quiz_game_page.js
@@ -0,0 +1,19 @@
+function send() {
+    player1_name = document.getElementById.value; 
+    player2_name = document.getElementById.value; 
+
+actual_answer = parseInt(number1) * parent(number2)
+}
+
+question_number = "<h4>" + number1 + "X" + number + "</h4>";
+input_box = "<br>Answer : <input type='text' id='input_chack_box'>";
+check_button = "<br><br><button class='bnt bnt-info' onclick='check()'>Check</button>"
+question_number = "<h4>" + number1 + "X + number2 + "</h4>";
+input_box = "<br>Answer : <input type='text' id='input_check_box'>"; 
+check_button = "<br><br><button class='btn btn-info' onclick='check()'>Check</button>";
+row = question_number| + input_box + check_button ;
+
+
+document.getElementById("output").innerHTML = row;
+document.getElementById("number1").value = "";
+document.getElementById("number2").value = "";
